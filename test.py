f6ce00a3d3fe9998075c64bcebeb2b3c1a457f13
manantessact <50653504+manantessact@users.noreply.github.com>
2019-09-18 15:23:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ while 1:
   after = dict ([(f, None) for f in os.listdir (path_to_watch)])
   added = [f for f in after if not f in before]
   removed = [f for f in before if not f in after]
-  if added: print "Added: ", ", ".join (added)
-  if removed: print "Removed: ", ", ".join (removed)
+  if added: print("Added: ", ", ".join (added))
+  if removed: print("Removed: ", ", ".join (removed))
   before = after
