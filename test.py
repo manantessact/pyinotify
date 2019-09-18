bcd9ded3cc46d83d1a2ae182db2a756fb3592060
manantessact <50653504+manantessact@users.noreply.github.com>
2019-09-18 15:05:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,28 +2,28 @@ import pyinotify
 
 class MyEventHandler(pyinotify.ProcessEvent):
     def process_IN_ACCESS(self, event):
-        print "ACCESS event:", event.pathname
+        print ("ACCESS event:", event.pathname)
 
     def process_IN_ATTRIB(self, event):
-        print "ATTRIB event:", event.pathname
+        print ("ATTRIB event:", event.pathname)
 
     def process_IN_CLOSE_NOWRITE(self, event):
-        print "CLOSE_NOWRITE event:", event.pathname
+        print ("CLOSE_NOWRITE event:", event.pathname)
 
     def process_IN_CLOSE_WRITE(self, event):
-        print "CLOSE_WRITE event:", event.pathname
+        print ("CLOSE_WRITE event:", event.pathname)
 
     def process_IN_CREATE(self, event):
-        print "CREATE event:", event.pathname
+        print ("CREATE event:", event.pathname)
 
     def process_IN_DELETE(self, event):
-        print "DELETE event:", event.pathname
+        print ("DELETE event:", event.pathname)
 
     def process_IN_MODIFY(self, event):
-        print "MODIFY event:", event.pathname
+        print ("MODIFY event:", event.pathname)
 
     def process_IN_OPEN(self, event):
-        print "OPEN event:", event.pathname
+        print ("OPEN event:", event.pathname)
 
 def main():
     # watch manager
