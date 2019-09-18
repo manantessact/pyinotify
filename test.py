7405aeea90a4172f70a46b545d03907ac2137f04
manantessact <50653504+manantessact@users.noreply.github.com>
2019-09-18 15:26:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import os, time
-path_to_watch = "."
+path_to_watch = "/sharepoint/TO_TRANSCODE/"
 before = dict ([(f, None) for f in os.listdir (path_to_watch)])
 while 1:
-  time.sleep (10)
+  time.sleep (2)
   after = dict ([(f, None) for f in os.listdir (path_to_watch)])
   added = [f for f in after if not f in before]
   removed = [f for f in before if not f in after]
