ffd5daf6dd5f56223215fdfbdb313533a370c90e
manantessact <50653504+manantessact@users.noreply.github.com>
2019-09-20 14:20:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,44 @@
-import os, time
-path_to_watch = "/sharepoint/TO_TRANSCODE/"
-before = dict ([(f, None) for f in os.listdir (path_to_watch)])
-while 1:
-  time.sleep (2)
-  after = dict ([(f, None) for f in os.listdir (path_to_watch)])
-  added = [f for f in after if not f in before]
-  removed = [f for f in before if not f in after]
-  if added: print("Added: ", ", ".join (added))
-  if removed: print("Removed: ", ", ".join (removed))
-  before = after
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Watcher:
+    DIRECTORY_TO_WATCH = "/path/to/my/directory"
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print "Error"
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print("Received created event - %s." % event.src_path)
+
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.
+            print("Received modified event - %s." % event.src_path)
+
+
+if __name__ == '__main__':
+    w = Watcher()
+    w.run()
