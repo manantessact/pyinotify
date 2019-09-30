35df4aca5c18b0539a27eceda59167a796bec1cb
manantessact <50653504+manantessact@users.noreply.github.com>
2019-09-30 15:38:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os, time
-path_to_watch = "."
+path_to_watch = "/sharepoint/TO_TRANSCODE/"
 before = dict ([(f, None) for f in os.listdir (path_to_watch)])
 while 1:
     time.sleep (1)
@@ -8,6 +8,7 @@ while 1:
     removed = [f for f in before if not f in after]
     if added:
         for vid in added:
+            vid = path_to_watch + vid
             file_size = -1
             while file_size != os.path.getsize(vid):
                 file_size = os.path.getsize(vid)
