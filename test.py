726a578084b11ac5f55847bbf3e55fb06ada63da
manantessact <50653504+manantessact@users.noreply.github.com>
2019-09-30 15:32:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,16 @@
-import sys
-import time
-import logging
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+import os, time
+path_to_watch = "."
+before = dict ([(f, None) for f in os.listdir (path_to_watch)])
+while 1:
+    time.sleep (1)
+    after = dict ([(f, None) for f in os.listdir (path_to_watch)])
+    added = [f for f in after if not f in before]
+    removed = [f for f in before if not f in after]
+    if added:
+        for vid in added:
+            file_size = -1
+            while file_size != os.path.getsize(event.src_path):
+                file_size = os.path.getsize(event.src_path)
+                time.sleep(1)
+            print("added : {}".format(vid))
+    before = after
