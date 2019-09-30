3a8a25391c308599b0b593fbd89ba2d45d7740bc
manantessact <50653504+manantessact@users.noreply.github.com>
2019-09-30 14:19:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,67 +1,21 @@
 import sys
 import time
-
+import logging
 from watchdog.observers import Observer
-
-import os
-from PIL import Image
-from PIL.ImageOps import grayscale
-from watchdog.events import RegexMatchingEventHandler
-
-class ImagesEventHandler(RegexMatchingEventHandler):
-    THUMBNAIL_SIZE = (128, 128)
-    IMAGES_REGEX = [r".*[^_thumbnail]\.jpg$"]
-
-    def __init__(self):
-        super().__init__(self.IMAGES_REGEX)
-
-    def on_created(self, event):
-        file_size = -1
-        while file_size != os.path.getsize(event.src_path):
-            file_size = os.path.getsize(event.src_path)
-            time.sleep(1)
-        print("File found")
-        self.process(event)
-
-    def process(self, event):
-        filename, ext = os.path.splitext(event.src_path)
-        filename = f"{filename}_thumbnail.jpg"
-
-        image = Image.open(event.src_path)
-        image = grayscale(image)
-        image.thumbnail(self.THUMBNAIL_SIZE)
-        image.save(filename)
-
-
-class ImagesWatcher:
-    def __init__(self, src_path):
-        self.__src_path = src_path
-        self.__event_handler = ImagesEventHandler()
-        self.__event_observer = Observer()
-
-    def run(self):
-        self.start()
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            self.stop()
-
-    def start(self):
-        self.__schedule()
-        self.__event_observer.start()
-
-    def stop(self):
-        self.__event_observer.stop()
-        self.__event_observer.join()
-
-    def __schedule(self):
-        self.__event_observer.schedule(
-            self.__event_handler,
-            self.__src_path,
-            recursive=True
-        )
+from watchdog.events import LoggingEventHandler
 
 if __name__ == "__main__":
-    src_path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    ImagesWatcher(src_path).run()
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
