f81b90a9ff4fd9bb2c9dca59f376575be116a20d
manantessact <50653504+manantessact@users.noreply.github.com>
2019-09-30 13:41:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,44 +1,67 @@
+import sys
 import time
+
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
+import os
+from PIL import Image
+from PIL.ImageOps import grayscale
+from watchdog.events import RegexMatchingEventHandler
 
-class Watcher:
-    DIRECTORY_TO_WATCH = "/path/to/my/directory"
+class ImagesEventHandler(RegexMatchingEventHandler):
+    THUMBNAIL_SIZE = (128, 128)
+    IMAGES_REGEX = [r".*[^_thumbnail]\.jpg$"]
 
     def __init__(self):
-        self.observer = Observer()
+        super().__init__(self.IMAGES_REGEX)
+
+    def on_created(self, event):
+    file_size = -1
+    while file_size != os.path.getsize(event.src_path):
+        file_size = os.path.getsize(event.src_path)
+        time.sleep(1)
+    print("File found")
+    self.process(event)
+
+    def process(self, event):
+        filename, ext = os.path.splitext(event.src_path)
+        filename = f"{filename}_thumbnail.jpg"
+
+        image = Image.open(event.src_path)
+        image = grayscale(image)
+        image.thumbnail(self.THUMBNAIL_SIZE)
+        image.save(filename)
+
+
+class ImagesWatcher:
+    def __init__(self, src_path):
+        self.__src_path = src_path
+        self.__event_handler = ImagesEventHandler()
+        self.__event_observer = Observer()
 
     def run(self):
-        event_handler = Handler()
-        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
-        self.observer.start()
+        self.start()
         try:
             while True:
-                time.sleep(5)
-        except:
-            self.observer.stop()
-            print "Error"
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.stop()
 
-        self.observer.join()
+    def start(self):
+        self.__schedule()
+        self.__event_observer.start()
 
+    def stop(self):
+        self.__event_observer.stop()
+        self.__event_observer.join()
 
-class Handler(FileSystemEventHandler):
+    def __schedule(self):
+        self.__event_observer.schedule(
+            self.__event_handler,
+            self.__src_path,
+            recursive=True
+        )
 
-    @staticmethod
-    def on_any_event(event):
-        if event.is_directory:
-            return None
-
-        elif event.event_type == 'created':
-            # Take any action here when a file is first created.
-            print("Received created event - %s." % event.src_path)
-
-        elif event.event_type == 'modified':
-            # Taken any action here when a file is modified.
-            print("Received modified event - %s." % event.src_path)
-
-
-if __name__ == '__main__':
-    w = Watcher()
-    w.run()
+if __name__ == "__main__":
+    src_path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    ImagesWatcher(src_path).run()
